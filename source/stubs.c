9ee270c2d8a4b349434ca40e75150b8099a0333a
Maldus512 <mattia512maldini@gmail.com>
2017-12-29 18:52:54 +0100

    typo

--- source/stubs.c
+++ source/stubs.c
@@ -12,7 +12,6 @@ caddr_t _sbrk( int incr ) {
     extern char _end;
     static char* heap_end = 0;
     char* prev_heap_end;
-    heap_end = 0x11ed0;
 
     if( heap_end == 0 )
         heap_end = &_end;
